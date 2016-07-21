fe906cd8cf57c19ac281d56581752590621100c2
Alex Smith <alex.smith@redhat.com>
2016-07-21 12:36:37 -0400

    Configure Grafana repo for RPM installation.

--- fedora/install_packages.sh
+++ fedora/install_packages.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-dnf install -y \
-    https://grafanarel.s3.amazonaws.com/builds/grafana-3.1.0-1468321182.x86_64.rpm
+dnf install -y grafana-3.1.0
 dnf clean all
--- rhel/install_packages.sh
+++ rhel/install_packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-yum install -y --disablerepo=* --enablerepo=rhel-7-server-rpms \
-    https://grafanarel.s3.amazonaws.com/builds/grafana-3.1.0-1468321182.x86_64.rpm
+yum install -y \
+    --disablerepo=* \
+    --enablerepo=rhel-7-server-rpms \
+    --enablerepo=grafana \
+    grafana-3.1.0
 yum clean all
